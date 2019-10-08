5741492f0ae5d98f622c345f0ce794cf997b9bcf
wyx <924931865@qq.com>
2019-10-08 19:21:18 +0800

    QPainter.DrawText-1008

---
+++ 绘图/DrawText.py
@@ -0,0 +1,46 @@
+'''
+绘图API可以绘制
+1、文本
+2、各种图形（直线、点、椭圆、弧形、扇形、多边形）
+3、图像
+
+使用QPainter 类
+过程：
+1、实例化一个QPainter
+2、painter.begin()
+3、painter.drawText(...)
+4、painter.end()
+必须在paintEvent事件方法中绘制各种元素,paintEvent事件在创建窗口或窗口尺寸变化时调用。当窗口尺寸变化时，会重新绘制窗口
+
+'''
+
+import sys
+from PyQt5.QtWidgets import QWidget,QApplication
+from PyQt5.QtGui import QPainter,QColor,QFont
+from PyQt5.QtCore import Qt
+
+
+class DrawText(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('绘制文本演示')
+        self.resize(500,400)
+        self.text = 'Hello,这是一个例子'
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.begin(self)
+        print('开始绘制')
+        painter.setPen(QColor(155,55,120))
+        painter.setFont(QFont('Arial',20))
+
+        painter.drawText(event.rect(),Qt.AlignCenter,self.text)
+
+        painter.end()
+
+
+if __name__ =="__main__":
+    app = QApplication(sys.argv)
+    main = DrawText()
+    main.show()
+    sys.exit(app.exec_())
